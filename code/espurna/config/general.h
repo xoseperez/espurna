8ad3c8708e74b9a47dc032a65e879a9ead4c7320
Xose Pérez <xose.perez@gmail.com>
2017-01-14 00:16:24 +0100

    Document fauxmo and domoticz default settings

--- code/espurna/config/general.h
+++ code/espurna/config/general.h
@@ -89,7 +89,9 @@
 // DOMOTICZ
 // -----------------------------------------------------------------------------
 
-#define ENABLE_DOMOTICZ         1
+#ifndef ENABLE_DOMOTICZ
+    #define ENABLE_DOMOTICZ     1
+#endif
 #define DOMOTICZ_IN_TOPIC       "domoticz/in"
 #define DOMOTICZ_OUT_TOPIC      "domoticz/out"
 
@@ -106,7 +108,12 @@
 // FAUXO
 // -----------------------------------------------------------------------------
 
+// This setting defines whether Alexa support should be built into the firmware
 #ifndef ENABLE_FAUXMO
     #define ENABLE_FAUXMO       1
 #endif
+
+// This is default value for the fauxmoEnabled setting that defines whether
+// this device should be discoberable and respond to Alexa commands.
+// Both ENABLE_FAUXMO and fauxmoEnabled should be 1 for Alexa support to work.
 #define FAUXMO_ENABLED          1
