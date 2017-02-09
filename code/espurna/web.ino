2f12e66b79b2861fa0545fb80c210f8cb4199df5
Toni Arte <toni.arte@iki.fi>
2017-02-09 12:41:15 +0200

    Add a separate incremental energy counter into the Domoticz MQTT interface

--- code/espurna/web.ino
+++ code/espurna/web.ino
@@ -350,12 +350,14 @@ void _wsStart(uint32_t client_id) {
 
         #if ENABLE_EMON
             root["dczPowIdx"] = getSetting("dczPowIdx").toInt();
+            root["dczEnergyIdx"] = getSetting("dczEnergyIdx").toInt();
             root["dczVoltIdx"] = getSetting("dczVoltIdx").toInt();
             root["dczCurrentIdx"] = getSetting("dczCurrentIdx").toInt();
         #endif
 
         #if ENABLE_POW
             root["dczPowIdx"] = getSetting("dczPowIdx").toInt();
+            root["dczEnergyIdx"] = getSetting("dczEnergyIdx").toInt();
             root["dczVoltIdx"] = getSetting("dczVoltIdx").toInt();
             root["dczCurrentIdx"] = getSetting("dczCurrentIdx").toInt();
         #endif
