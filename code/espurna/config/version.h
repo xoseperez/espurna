5c8eebe12e2604c894b14a27464fe718604f5d32
Xose Pérez <xose.perez@gmail.com>
2017-02-28 22:58:02 +0100

    Updating staging version number

--- code/espurna/config/version.h
+++ code/espurna/config/version.h
@@ -1,4 +1,4 @@
 #define APP_NAME                "ESPurna"
-#define APP_VERSION             "1.6.7"
+#define APP_VERSION             "1.6.8b"
 #define APP_AUTHOR              "xose.perez@gmail.com"
 #define APP_WEBSITE             "http://tinkerman.cat"
