f45db831c030fc7e97756c955a674e0b4481794c
Xose Pérez <xose.perez@gmail.com>
2017-03-05 23:09:21 +0100

    Added custom.h entry in the all.h file. See issue #104

--- code/espurna/config/all.h
+++ code/espurna/config/all.h
@@ -5,3 +5,12 @@
 #include "general.h"
 #include "hardware.h"
 #include "sensors.h"
+
+/*
+    If you want to modify the stock configuration but you don't want to touch
+    the repo files you can uncomment the following line and add a "custom.h"
+    file to this same folder.
+    Check https://bitbucket.org/xoseperez/espurna/issues/104/general_customh
+    for an example on how to use this file.
+*/
+//#include "custom.h"
