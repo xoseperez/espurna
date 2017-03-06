8cc1aae248151bc4c5ae0966ad85894e9e4e60d6
Xose Pérez <xose.perez@gmail.com>
2017-03-06 11:59:10 +0100

    Added optional debug via UDP

--- code/espurna/config/debug.h
+++ code/espurna/config/debug.h
@@ -1,8 +1,12 @@
-#ifdef DEBUG_PORT
-#ifndef SONOFF_DUAL
-    #define DEBUG_MSG(...) DEBUG_PORT.printf( __VA_ARGS__ )
-    #define DEBUG_MSG_P(...) { char buffer[81]; snprintf_P(buffer, 80, __VA_ARGS__ ); DEBUG_PORT.printf( buffer ); }
+#define DEBUG_MESSAGE_MAX_LENGTH    80
+
+#ifdef SONOFF_DUAL
+#undef DEBUG_PORT
 #endif
+
+#if defined(DEBUG_PORT) | defined(DEBUG_UDP_IP)
+    #define DEBUG_MSG(...) debugSend(__VA_ARGS__)
+    #define DEBUG_MSG_P(...) debugSend_P(__VA_ARGS__)
 #endif
 
 #ifndef DEBUG_MSG
---
+++ code/espurna/debug.ino
@@ -0,0 +1,60 @@
+/*
+
+DEBUG MODULE
+
+Copyright (C) 2016-2017 by Xose Pérez <xose dot perez at gmail dot com>
+
+*/
+
+#include <stdio.h>
+#include <stdarg.h>
+
+#ifdef DEBUG_UDP_IP
+#include <WiFiUdp.h>
+WiFiUDP udpDebug;
+#endif
+
+void debugSend(const char * format, ...) {
+
+    char buffer[DEBUG_MESSAGE_MAX_LENGTH+1];
+
+    va_list args;
+    va_start(args, format);
+    ets_vsnprintf(buffer, DEBUG_MESSAGE_MAX_LENGTH, format, args);
+    va_end(args);
+
+    #ifdef DEBUG_PORT
+        DEBUG_PORT.printf(buffer);
+    #endif
+
+    #ifdef DEBUG_UDP_IP
+        udpDebug.beginPacket(DEBUG_UDP_IP, DEBUG_UDP_PORT);
+        udpDebug.write(buffer);
+        udpDebug.endPacket();
+    #endif
+
+}
+
+void debugSend_P(PGM_P format, ...) {
+
+    char buffer[DEBUG_MESSAGE_MAX_LENGTH+1];
+
+    char f[DEBUG_MESSAGE_MAX_LENGTH+1];
+    memcpy_P(f, format, DEBUG_MESSAGE_MAX_LENGTH);
+
+    va_list args;
+    va_start(args, format);
+    ets_vsnprintf(buffer, DEBUG_MESSAGE_MAX_LENGTH, f, args);
+    va_end(args);
+
+    #ifdef DEBUG_PORT
+        DEBUG_PORT.printf(buffer);
+    #endif
+
+    #ifdef DEBUG_UDP_IP
+        udpDebug.beginPacket(DEBUG_UDP_IP, DEBUG_UDP_PORT);
+        udpDebug.write(buffer);
+        udpDebug.endPacket();
+    #endif
+
+}
