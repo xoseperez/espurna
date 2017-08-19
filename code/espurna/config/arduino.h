7d09672887de38cdd3c3b0425aedce8572042717
Xose Pérez <xose.perez@gmail.com>
2017-08-19 16:51:04 +0200

    Update arduino.h

--- code/espurna/config/arduino.h
+++ code/espurna/config/arduino.h
@@ -44,14 +44,18 @@
 // Features (values below are non-default values)
 //--------------------------------------------------------------------------------
 
+//#define ENABLE_ANALOG         1
 //#define ENABLE_DHT            1
+//#define ENABLE_DOMOTICZ       0
 //#define ENABLE_DS18B20        1
 //#define ENABLE_EMON           1
 //#define ENABLE_HLW8012        1
-//#define ENABLE_RF             1
-//#define ENABLE_FAUXMO         0
-//#define ENABLE_NOFUSS         1
-//#define ENABLE_DOMOTICZ       0
-//#define ENABLE_ANALOG         1
-//#define ENABLE_INFLUXDB       0
 //#define ENABLE_I2C            1
+//#define ENABLE_INFLUXDB       0
+//#define ENABLE_FAUXMO         0
+//#define ENABLE_MDNS           0
+//#define ENABLE_NOFUSS         1
+//#define ENABLE_RF             1
+//#define ENABLE_SERIAL_DEBUG   0
+//#define ENABLE_TERMINAL       0
+//#define ENABLE_UDP_DEBUG      1
