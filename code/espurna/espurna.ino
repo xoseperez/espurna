0e8d2c191d138e614790825ac89d371cf9c140c6
soif <wxopwx@gmail.com>
2017-10-05 16:20:01 +0200

    Implements lightLoop + Includes (WTH) FastLed

--- code/espurna/espurna.ino
+++ code/espurna/espurna.ino
@@ -21,6 +21,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "config/all.h"
 #include <EEPROM.h>
+#include <FastLED.h>    // W.T.H. is this include ALSO needed here ?!?!!
 
 // -----------------------------------------------------------------------------
 // METHODS
@@ -302,6 +303,11 @@ void loop() {
 
     // Do not run the next services if system is flagged stable
     if (!systemCheck()) return;
+
+    #if LIGHT_PROVIDER != LIGHT_PROVIDER_NONE
+        lightLoop();
+    #endif
+
 
     buttonLoop();
     relayLoop();
