9ad78c8026b02901d426ff701d6778296ab2e834
Xose Pérez <xose.perez@gmail.com>
2017-11-26 18:56:46 +0100

    Beta version 1.10.1b

--- code/espurna/config/version.h
+++ code/espurna/config/version.h
@@ -1,4 +1,4 @@
 #define APP_NAME                "ESPURNA"
-#define APP_VERSION             "1.10.0"
+#define APP_VERSION             "1.10.1b"
 #define APP_AUTHOR              "xose.perez@gmail.com"
 #define APP_WEBSITE             "http://tinkerman.cat"
