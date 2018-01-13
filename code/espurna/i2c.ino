a864df1c529eac076995ee57df25a3a783405ecb
Xose Pérez <xose.perez@gmail.com>
2018-01-13 15:21:33 +0100

    Move i2c settings

--- code/espurna/i2c.ino
+++ code/espurna/i2c.ino
@@ -185,13 +185,15 @@ void i2cSetup() {
 
     unsigned char sda = getSetting("i2cSDA", I2C_SDA_PIN).toInt();
     unsigned char scl = getSetting("i2cSCL", I2C_SCL_PIN).toInt();
+    unsigned long cst = getSetting("i2cCST", I2C_CLOCK_STRETCH_TIME).toInt();
+    _i2c_scl_frequency = getSetting("i2cFreq", I2C_SCL_FREQUENCY).toInt();
 
     #if I2C_USE_BRZO
-        unsigned long cst = getSetting("i2cCST", I2C_CLOCK_STRETCH_TIME).toInt();
-        _i2c_scl_frequency = getSetting("i2cFreq", I2C_SCL_FREQUENCY).toInt();
         brzo_i2c_setup(sda, scl, cst);
     #else
         Wire.begin(sda, scl);
+        //Wire.setClock(_i2c_scl_frequency);
+        //Wire.setClockStretchLimit(cst);
     #endif
 
     DEBUG_MSG_P(PSTR("[I2C] Using GPIO%d for SDA and GPIO%d for SCL\n"), sda, scl);
