f9a478f885669e9de2975bb8756fd2fd37dab96e
AlbertWeterings <36169962+AlbertWeterings@users.noreply.github.com>
2018-02-20 06:21:19 +0100

    added UARTtoMQTT

--- code/espurna/espurna.ino
+++ code/espurna/espurna.ino
@@ -147,6 +147,10 @@ void setup() {
     #if SCHEDULER_SUPPORT
         schSetup();
     #endif
+    #ifdef UARTtoMQTT
+        UARTtoMQTTSetup();
+    #endif
+
 
     // 3rd party code hook
     #if USE_EXTRA
