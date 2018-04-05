3c04bafcf2f957356d5198abf30aafd323b10c8e
Lazar Obradovic <laz.obradovic@gmail.com>
2018-04-05 18:38:30 +0800

    stub for custom config and custom boards

---
+++ code/espurna/config/custom.h
@@ -0,0 +1,35 @@
+// -----------------------------------------------------------------------------
+// Your custom board definitions.
+// -----------------------------------------------------------------------------
+
+
+// Global stuff
+#define WIFI1_SSID              "myssid"
+#define WIFI1_PASS              "mypassword"
+
+#if defined(CUSTOM01)
+
+    // Info
+    #define MANUFACTURER            "ESPRESSIF"
+    #define DEVICE                  "ESPURNA_CORE"
+
+    // Add your buttons, relays, lights and sensors definitions and pinout here
+
+#elif defined(CUSTOM02)
+
+    // Info
+    #define MANUFACTURER            "ESPRESSIF"
+    #define DEVICE                  "ESPURNA_CORE"
+
+    // Add your buttons, relays, lights and sensors definitions and pinout here
+
+
+#elif defined(CUSTOM03)
+
+    // Info
+    #define MANUFACTURER            "ESPRESSIF"
+    #define DEVICE                  "ESPURNA_CORE"
+
+    // Add your buttons, relays, lights and sensors definitions and pinout here
+
+#endif
