541c230411b1353abfe1eb8168dda4a719850efe
Lazar Obradovic <laz.obradovic@gmail.com>
2018-04-05 19:52:28 +0800

    better naming for custom device templates

--- code/espurna/config/custom.h
+++ code/espurna/config/custom.h
@@ -10,16 +10,16 @@
 #if defined(CUSTOM01)
 
     // Info
-    #define MANUFACTURER            "ESPRESSIF"
-    #define DEVICE                  "ESPURNA_CORE"
+    #define MANUFACTURER            "CUSTOM"
+    #define DEVICE                  "My device 01"
 
     // Add your buttons, relays, lights and sensors definitions and pinout here
 
 #elif defined(CUSTOM02)
 
     // Info
-    #define MANUFACTURER            "ESPRESSIF"
-    #define DEVICE                  "ESPURNA_CORE"
+    #define MANUFACTURER            "CUSTOM"
+    #define DEVICE                  "My device 02"
 
     // Add your buttons, relays, lights and sensors definitions and pinout here
 
@@ -27,8 +27,8 @@
 #elif defined(CUSTOM03)
 
     // Info
-    #define MANUFACTURER            "ESPRESSIF"
-    #define DEVICE                  "ESPURNA_CORE"
+    #define MANUFACTURER            "CUSTOM"
+    #define DEVICE                  "My device 02"
 
     // Add your buttons, relays, lights and sensors definitions and pinout here
 
