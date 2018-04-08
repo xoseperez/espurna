4d9899bd26a93d62fa81e82b4af4048b757b6d30
Xose Pérez <xose.perez@gmail.com>
2018-04-08 22:47:49 +0200

    Hide web command inputbox if no terminal support

--- code/espurna/ws.ino
+++ code/espurna/ws.ino
@@ -298,7 +298,9 @@ void _wsOnStart(JsonObject& root) {
         root["btnDelay"] = getSetting("btnDelay", BUTTON_DBLCLICK_DELAY).toInt();
         root["webPort"] = getSetting("webPort", WEB_PORT).toInt();
         root["wsAuth"] = getSetting("wsAuth", WS_AUTHENTICATION).toInt() == 1;
-        root["cmdVisible"] = TERMINAL_SUPPORT;
+        #if TERMINAL_SUPPORT
+            root["cmdVisible"] = 1;
+        #endif
 
     }
 
