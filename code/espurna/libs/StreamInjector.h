88b25a4d5c41febe4e5783f465d3082108a06bc7
Xose Pérez <xose.perez@gmail.com>
2018-04-18 13:52:54 +0200

    StreamInjector header

--- code/espurna/libs/StreamInjector.h
+++ code/espurna/libs/StreamInjector.h
@@ -1,6 +1,23 @@
-// -----------------------------------------------------------------------------
-// Stream Injector
-// -----------------------------------------------------------------------------
+/*
+
+StreamInjector
+
+Copyright (C) 2016-2018 by Xose Pérez <xose dot perez at gmail dot com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
 
 #pragma once
 
@@ -35,11 +52,11 @@ class StreamInjector : public Stream {
             return len;
         }
 
-        // ---------------------------------------------------------------------
-
         virtual void callback(writeCallback c) {
             _callback = c;
         }
+
+        // ---------------------------------------------------------------------
 
         virtual size_t write(uint8_t ch) {
             if (_callback) _callback(ch);
