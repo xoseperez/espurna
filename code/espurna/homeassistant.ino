47351744809217e8c7b3e12599525655e2c048b5
Maxim Prokhorov <prokhorov.max@outlook.com>
2018-05-12 22:55:44 +0300

    "sensor" is not supported device_class

--- code/espurna/homeassistant.ino
+++ code/espurna/homeassistant.ino
@@ -24,7 +24,6 @@ void _haSendMagnitude(unsigned char i, JsonObject& config) {
     unsigned char type = magnitudeType(i);
     config["name"] = getSetting("hostname") + String(" ") + magnitudeTopic(type);
     config.set("platform", "mqtt");
-    config.set("device_class", "sensor");
     config["state_topic"] = mqttTopic(magnitudeTopicIndex(i).c_str(), false);
     config["unit_of_measurement"] = String("\"") + magnitudeUnits(type) + String("\"");
 
