c19cd166b229ef346915e3777dd90e218a4deffa
Xose Pérez <xose.perez@gmail.com>
2018-06-23 20:06:15 +0200

    Set version 1.13.1a

--- code/espurna/config/version.h
+++ code/espurna/config/version.h
@@ -1,5 +1,5 @@
 #define APP_NAME                "ESPURNA"
-#define APP_VERSION             "1.13.0"
+#define APP_VERSION             "1.13.1a"
 #define APP_REVISION            "d543fee"
 #define APP_AUTHOR              "xose.perez@gmail.com"
 #define APP_WEBSITE             "http://tinkerman.cat"
