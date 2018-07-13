f80b05a485789c1edf97d3083defe9d0d4a61b46
vtochq <vtochq@gmail.com>
2018-07-13 14:42:12 +0600

    Update hardware.h
    add SMARTLIFE 2CH switch support (black clone of Itead product)

--- code/espurna/config/hardware.h
+++ code/espurna/config/hardware.h
@@ -2595,6 +2595,64 @@
     #define NETBIOS_SUPPORT     1
     #define SSDP_SUPPORT        1
 
+#elif defined(SMARTLIFE_T1_2CH)
+//used pins 2 3 5 12 13 15 16
+//free pins 14 0 4
+    // Info
+    #define MANUFACTURER        "SMARTLIFE"
+    #define DEVICE              "SMARTLIFE_T1_2CH"
+
+    // Buttons
+    #define BUTTON1_PIN         12
+    #define BUTTON2_PIN         5
+    //#define BUTTON3_PIN         3
+
+    #define BUTTON1_MODE        BUTTON_PUSHBUTTON | BUTTON_DEFAULT_HIGH
+    #define BUTTON1_PRESS       BUTTON_MODE_TOGGLE
+    #define BUTTON1_CLICK       BUTTON_MODE_NONE
+    #define BUTTON1_DBLCLICK    BUTTON_MODE_NONE
+    #define BUTTON1_LNGCLICK    BUTTON_MODE_NONE
+    #define BUTTON1_LNGLNGCLICK BUTTON_MODE_RESET
+
+    #define BUTTON2_MODE        BUTTON_PUSHBUTTON | BUTTON_DEFAULT_HIGH
+    #define BUTTON2_PRESS       BUTTON_MODE_TOGGLE
+    #define BUTTON2_CLICK       BUTTON_MODE_NONE
+    #define BUTTON2_DBLCLICK    BUTTON_MODE_NONE
+    #define BUTTON2_LNGCLICK    BUTTON_MODE_NONE
+    #define BUTTON2_LNGLNGCLICK BUTTON_MODE_RESET
+
+    #define BUTTON3_MODE        BUTTON_PUSHBUTTON | BUTTON_DEFAULT_HIGH
+    #define BUTTON3_PRESS       BUTTON_MODE_TOGGLE
+    #define BUTTON3_CLICK       BUTTON_MODE_NONE
+    #define BUTTON3_DBLCLICK    BUTTON_MODE_NONE
+    #define BUTTON3_LNGCLICK    BUTTON_MODE_NONE
+    #define BUTTON3_LNGLNGCLICK BUTTON_MODE_RESET
+
+
+    #define BUTTON1_RELAY       1
+    #define BUTTON2_RELAY       2
+
+    // Relays
+    #define RELAY1_PIN          13
+    #define RELAY2_PIN          15
+
+    #define RELAY1_TYPE         RELAY_TYPE_NORMAL
+    #define RELAY2_TYPE         RELAY_TYPE_NORMAL
+
+    #define LED2_RELAY          1
+    #define LED3_RELAY          2
+
+    // LEDs
+    #define LED2_PIN            16
+    #define LED2_PIN_INVERSE    1
+    #define LED3_PIN            2
+    #define LED3_PIN_INVERSE    1
+    #define LED2_MODE         LED_MODE_FOLLOW_INVERSE
+    #define LED3_MODE         LED_MODE_FOLLOW_INVERSE
+
+    #define LED1_PIN            0
+    #define LED1_PIN_INVERSE    0
+
 #endif
 
 // -----------------------------------------------------------------------------
