d689d6db719801bea7b0dda7a2c6f65a9c4de5a7
Peter Hoeg <peter@hoeg.com>
2018-11-13 15:52:15 +0800

    allow override of HOMEASSISTANT_ENABLED and _PREFIX

--- code/espurna/config/general.h
+++ code/espurna/config/general.h
@@ -927,8 +927,13 @@
 #define HOMEASSISTANT_SUPPORT   MQTT_SUPPORT    // Build with home assistant support (if MQTT, 1.64Kb)
 #endif
 
+#ifndef HOMEASSISTANT_ENABLED
 #define HOMEASSISTANT_ENABLED   0               // Integration not enabled by default
+#endif
+
+#ifndef HOMEASSISTANT_PREFIX
 #define HOMEASSISTANT_PREFIX    "homeassistant" // Default MQTT prefix
+#endif
 
 #ifndef HOMEASSISTANT_PAYLOAD_ON
 #define HOMEASSISTANT_PAYLOAD_ON    "1"         // Payload for ON and available messages
