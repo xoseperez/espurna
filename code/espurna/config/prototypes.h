103d8836df5320cf97b5fec0a814b4afbe89f98d
Max Prokhorov <prokhorov.max@outlook.com>
2018-11-27 11:21:23 +0300

    Port PROGMEM from 2.5.0

--- code/espurna/config/prototypes.h
+++ code/espurna/config/prototypes.h
@@ -8,6 +8,14 @@ extern "C" {
     #include "user_interface.h"
 }
 
+// ref: esp8266/Arduino/cores/esp8266/pgmspace.h @ 2.5.0
+// __STRINGIZE && __STRINGIZE_NX && PROGMEM definitions port
+#define __TO_STR_(A) #A
+#define __TO_STR(A) __TO_STR_(A)
+
+#undef PROGMEM
+#define PROGMEM      __attribute__((section( "\".irom.text." __FILE__ "." __TO_STR(__LINE__) "."  __TO_STR(__COUNTER__) "\"")))
+
 // -----------------------------------------------------------------------------
 // API
 // -----------------------------------------------------------------------------
