ee19446aaf2a119bc23fb90a13adf2bb084df67c
Max Prokhorov <prokhorov.max@outlook.com>
2019-02-08 01:27:47 +0300

    fix wsDebugSend prototype

--- code/espurna/config/prototypes.h
+++ code/espurna/config/prototypes.h
@@ -194,7 +194,7 @@ void webRequestRegister(web_request_callback_f callback);
     typedef std::function<bool(const char *, JsonVariant&)> ws_on_receive_callback_f;
     void wsOnReceiveRegister(ws_on_receive_callback_f callback);
 
-    bool wsDebugSend(const char*);
+    bool wsDebugSend(const char*, const char*);
 #else
     #define ws_on_send_callback_f void *
     #define ws_on_action_callback_f void *
