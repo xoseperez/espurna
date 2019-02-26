92e10943bb88ccc331c5af39f6504c799257819e
Xose Pérez <xose.perez@gmail.com>
2019-02-26 05:00:07 +0100

    Avoid websocket ping back on fw upgrade via web UI form (#1574)

--- code/espurna/web.ino
+++ code/espurna/web.ino
@@ -327,7 +327,8 @@ void _onUpgradeData(AsyncWebServerRequest *request, String filename, size_t inde
             #endif
         }
     } else {
-        DEBUG_MSG_P(PSTR("[UPGRADE] Progress: %u bytes\r"), index + len);
+        //Removed to avoid websocket ping back during upgrade (see #1574)
+        //DEBUG_MSG_P(PSTR("[UPGRADE] Progress: %u bytes\r"), index + len);
     }
 }
 
