57e5d94803a3e92770c2c79bcc38dee05b0bc5f5
Vencislav Atanasov <user890104@users.noreply.github.com>
2019-06-23 10:58:35 +0300

    Turn on/off LEDs using the push button

--- code/espurna/config/hardware.h
+++ code/espurna/config/hardware.h
@@ -1301,6 +1301,7 @@
     // Buttons
     #define BUTTON1_PIN         0
     #define BUTTON1_MODE        BUTTON_PUSHBUTTON | BUTTON_DEFAULT_HIGH
+    #define BUTTON1_RELAY       1
 
     // LEDs
     #define LED1_PIN            2
