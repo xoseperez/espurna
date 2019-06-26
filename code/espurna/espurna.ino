dcd8b066b948ff9c044ff16ae65620b323e6b74b
Max Prokhorov <prokhorov.max@outlook.com>
2019-06-26 11:20:38 +0300

    Update crash handler
    - limit recorder stack size by 128 bytes
    - offset settings a bit more
    - add possibility to disable crash crash recoder completely

--- code/espurna/espurna.ino
+++ code/espurna/espurna.ino
@@ -83,9 +83,13 @@ void setup() {
     // Init persistance
     settingsSetup();
 
+    // Init crash recorder
+    crashSetup();
+
     // Return bogus free heap value for broken devices
     // XXX: device is likely to trigger other bugs! tread carefuly
     wtfHeap(getSetting("wtfHeap", 0).toInt());
+
     // Init Serial, SPIFFS and system check
     systemSetup();
 
