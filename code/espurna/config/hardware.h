06043e3fa68b18b03121985738eb2837376d4049
Vencislav Atanasov <user890104@users.noreply.github.com>
2019-06-26 14:26:30 +0300

    Add config for push button of Arilux AL-LC06
    Button is not visible from the outside, but can be wired outside or made more accessible by using a thin object, so it feels more like a button than a hole. The configuration should not cause issues if the button is not used at all.

--- code/espurna/config/hardware.h
+++ code/espurna/config/hardware.h
@@ -1681,6 +1681,11 @@
     #define LIGHT_PROVIDER      LIGHT_PROVIDER_DIMMER
     #define DUMMY_RELAY_COUNT   1
 
+    // Buttons
+    #define BUTTON1_PIN         0
+    #define BUTTON1_MODE        BUTTON_PUSHBUTTON | BUTTON_DEFAULT_HIGH
+    #define BUTTON1_RELAY       1
+
     // Light
     #define LIGHT_CHANNELS      5
     #define LIGHT_CH1_PIN       14      // RED
