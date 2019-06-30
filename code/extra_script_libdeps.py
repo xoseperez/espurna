9bd417def9c5e1ec65f71861c415c93d4f5d333f
Max Prokhorov <prokhorov.max@outlook.com>
2019-06-30 03:18:29 +0300

    temporary fix py3 syntax, print to stderr

--- code/extra_script_libdeps.py
+++ code/extra_script_libdeps.py
@@ -1,6 +1,9 @@
+from __future__ import print_function
+
 Import("env")
 
 import os
+import sys
 
 def subprocess_libdeps(storage, lib_deps):
     import subprocess
@@ -13,11 +16,9 @@ def library_manager_libdeps(storage, lib_deps):
     from platformio.managers.lib import LibraryManager
     manager = LibraryManager(storage)
     for lib in lib_deps:
-        print(lib)
-        print(*manager.parse_pkg_uri(lib))
         if manager.get_package_dir(*manager.parse_pkg_uri(lib)):
             continue
-        print("installing", lib)
+        print("installing", lib, file=sys.stderr)
         manager.install(lib)
 
 if os.environ.get("ESPURNA_PIO_SHARED_LIBRARIES"):
@@ -26,6 +27,6 @@ if os.environ.get("ESPURNA_PIO_SHARED_LIBRARIES"):
     lib_deps = env.GetProjectOption("lib_deps")
     storage = os.path.join(env["PROJECT_DIR"], cfg.get("common", "shared_libdeps_dir"))
 
-    print("using shared library storage: ", storage)
+    print("using shared library storage: ", storage, file=sys.stderr)
     #library_manager_libdeps(STORAGE, LIB_DEPS)
     subprocess_libdeps(storage, lib_deps)
