eba000f5ea4736325db5a38410ca91e9ac3259c7
Pavel Slama <p.slama@atlas.cz>
2019-07-25 12:04:19 +0200

    deprecared pins

--- code/espurna/config/deprecated.h
+++ code/espurna/config/deprecated.h
@@ -13,3 +13,15 @@
 #warning RF_PIN is deprecated! Please use RFB_RX_PIN instead
 #define RFB_RX_PIN RF_PIN
 #endif
+
+// 1.13.6 allow multiple digitals
+#ifdef DIGITAL_PIN
+#warning DIGITAL_PIN is deprecated! Please use DIGITAL1_PIN instead
+#define DIGITAL1_PIN DIGITAL_PIN
+#endif
+
+// 1.13.6 allow multiple events
+#ifdef EVENTS_PIN
+#warning EVENTS_PIN is deprecated! Please use EVENTS1_PIN instead
+#define EVENTS1_PIN EVENTS_PIN
+#endif
