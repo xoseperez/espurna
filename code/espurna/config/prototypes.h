16979cd7d10e20da7ae2f436a71b621cc12838cc
Max Prokhorov <prokhorov.max@outlook.com>
2019-08-31 07:38:11 +0300

    ws: restore arg names in prototypes

--- code/espurna/config/prototypes.h
+++ code/espurna/config/prototypes.h
@@ -291,9 +291,9 @@ struct ws_data_t;
 struct ws_debug_t;
 struct ws_callbacks_t;
 
-using ws_on_send_callback_f = std::function<void(JsonObject&)>;
-using ws_on_action_callback_f = std::function<void(uint32_t, const char *, JsonObject&)>;
-using ws_on_keycheck_callback_f = std::function<bool(const char *, JsonVariant&)>;
+using ws_on_send_callback_f = std::function<void(JsonObject& root)>;
+using ws_on_action_callback_f = std::function<void(uint32_t client_id, const char * action, JsonObject& data)>;
+using ws_on_keycheck_callback_f = std::function<bool(const char * key, JsonVariant& value)>;
 
 using ws_on_send_callback_list_t = std::vector<ws_on_send_callback_f>;
 using ws_on_action_callback_list_t = std::vector<ws_on_action_callback_f>;
@@ -318,27 +318,27 @@ using ws_on_keycheck_callback_list_t = std::vector<ws_on_keycheck_callback_f>;
     ws_callbacks_t& wsRegister();
 
     void wsSetup();
-    void wsSend(uint32_t, const char*);
-    void wsSend(uint32_t, JsonObject&);
-    void wsSend(JsonObject&);
-    void wsSend(ws_on_send_callback_f);
+    void wsSend(uint32_t client_id, const char* data);
+    void wsSend(uint32_t client_id, JsonObject& root);
+    void wsSend(JsonObject& root);
+    void wsSend(ws_on_send_callback_f callback);
 
-    void wsSend_P(PGM_P);
-    void wsSend_P(uint32_t, PGM_P);
+    void wsSend_P(PGM_P data);
+    void wsSend_P(uint32_t client_id, PGM_P data);
 
-    void wsPost(const ws_on_send_callback_f&);
-    void wsPost(const ws_on_send_callback_list_t&);
-    void wsPost(uint32_t, const ws_on_send_callback_list_t&);
+    void wsPost(const ws_on_send_callback_f& callback);
+    void wsPost(const ws_on_send_callback_list_t& callbacks);
+    void wsPost(uint32_t client_id, const ws_on_send_callback_list_t& callbacks);
 
-    void wsPostAll(uint32_t, const ws_on_send_callback_list_t&);
-    void wsPostAll(const ws_on_send_callback_list_t&);
+    void wsPostAll(uint32_t client_id, const ws_on_send_callback_list_t& callbacks);
+    void wsPostAll(const ws_on_send_callback_list_t& callbacks);
 
-    void wsPostSequence(uint32_t, const ws_on_send_callback_list_t&);
-    void wsPostSequence(const ws_on_send_callback_list_t&);
+    void wsPostSequence(uint32_t client_id, const ws_on_send_callback_list_t& callbacks);
+    void wsPostSequence(const ws_on_send_callback_list_t& callbacks);
 
     bool wsConnected();
-    bool wsConnected(uint32_t);
-    bool wsDebugSend(const char*, const char*);
+    bool wsConnected(uint32_t client_id);
+    bool wsDebugSend(const char* prefix, const char* message);
 #endif
 
 // -----------------------------------------------------------------------------
