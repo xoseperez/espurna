919ba260a88ca8d8d28ff23e797a7b0f31bd7b7b
Max Prokhorov <prokhorov.max@outlook.com>
2019-10-08 21:01:53 +0300

    lights: switch to signed arithmetic, introduce adjustment and update step functions

--- code/espurna/light.h
+++ code/espurna/light.h
@@ -5,22 +5,22 @@
 #pragma once
 
 namespace Light {
-    constexpr const unsigned char VALUE_MIN = LIGHT_MIN_VALUE;
-    constexpr const unsigned char VALUE_MAX = LIGHT_MAX_VALUE;
+    constexpr const long VALUE_MIN = LIGHT_MIN_VALUE;
+    constexpr const long VALUE_MAX = LIGHT_MAX_VALUE;
 
-    constexpr const unsigned int BRIGHTNESS_MIN = LIGHT_MIN_BRIGHTNESS;
-    constexpr const unsigned int BRIGHTNESS_MAX = LIGHT_MAX_BRIGHTNESS;
+    constexpr const long BRIGHTNESS_MIN = LIGHT_MIN_BRIGHTNESS;
+    constexpr const long BRIGHTNESS_MAX = LIGHT_MAX_BRIGHTNESS;
 
     // Default to the Philips Hue value that HA also use.
     // https://developers.meethue.com/documentation/core-concepts
-    constexpr const unsigned int MIREDS_COLDWHITE = LIGHT_COLDWHITE_MIRED;
-    constexpr const unsigned int MIREDS_WARMWHITE = LIGHT_WARMWHITE_MIRED;
+    constexpr const long MIREDS_COLDWHITE = LIGHT_COLDWHITE_MIRED;
+    constexpr const long MIREDS_WARMWHITE = LIGHT_WARMWHITE_MIRED;
 
-    constexpr const unsigned int KELVIN_WARMWHITE = LIGHT_WARMWHITE_KELVIN;
-    constexpr const unsigned int KELVIN_COLDWHITE = LIGHT_COLDWHITE_KELVIN;
+    constexpr const long KELVIN_WARMWHITE = LIGHT_WARMWHITE_KELVIN;
+    constexpr const long KELVIN_COLDWHITE = LIGHT_COLDWHITE_KELVIN;
 
-    constexpr const unsigned int PWM_MIN = LIGHT_MIN_PWM;
-    constexpr const unsigned int PWM_MAX = LIGHT_MAX_PWM;
-    constexpr const unsigned int PWM_LIMIT = LIGHT_LIMIT_PWM;
+    constexpr const long PWM_MIN = LIGHT_MIN_PWM;
+    constexpr const long PWM_MAX = LIGHT_MAX_PWM;
+    constexpr const long PWM_LIMIT = LIGHT_LIMIT_PWM;
 }
 
