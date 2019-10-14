5693956172f8018cf30a6eb0207f4edf9e22781f
Maxim Prokhorov <prokhorov.max@outlook.com>
2019-10-14 18:28:13 +0300

    shellcheck

--- code/test_build.sh
+++ code/test_build.sh
@@ -6,7 +6,7 @@ CUSTOM_HEADER="espurna/config/custom.h"
 TARGET_ENVIRONMENT=${1:?"pio env name"}
 shift 1
 
-EXTRA_CONFIGURATIONS=$@
+EXTRA_CONFIGURATIONS=("$@")
 DEFAULT_CONFIGURATIONS=(
     basic
     sensor
@@ -18,17 +18,17 @@ DEFAULT_CONFIGURATIONS=(
 
 trap 'rm -f ${CUSTOM_HEADER}' EXIT
 
-for cfg in ${DEFAULT_CONFIGURATIONS[@]} ${EXTRA_CONFIGURATIONS} ; do
-    echo travis_fold:start:build_${cfg}
+for cfg in "${DEFAULT_CONFIGURATIONS[@]}" "${EXTRA_CONFIGURATIONS[@]}" ; do
+    echo "travis_fold:start:build_${cfg}"
     echo "- building ${cfg}"
 
-    printf "#define MANUFACTURER \"TEST_BUILD\"\n" | tee ${CUSTOM_HEADER}
-    printf "#define DEVICE \"${cfg^^}\"\n" | tee --append ${CUSTOM_HEADER}
-    cat test/build/${cfg}.h | tee --append ${CUSTOM_HEADER}
+    printf "#define MANUFACTURER \"%\"\n" "TEST_BUILD" | tee ${CUSTOM_HEADER}
+    printf "#define DEVICE \"%s\"\n" "${cfg^^}" | tee --append ${CUSTOM_HEADER}
+    tee --append ${CUSTOM_HEADER} < "test/build/${cfg}.h"
 
     export PLATFORMIO_SRC_BUILD_FLAGS="-DUSE_CUSTOM_H"
     export PLATFORMIO_BUILD_CACHE_DIR="test/pio_cache"
     
-    time pio run -s -e $TARGET_ENVIRONMENT
-    echo travis_fold:end:build_${cfg}
+    time pio run -e "$TARGET_ENVIRONMENT"
+    echo "travis_fold:end:build_${cfg}"
 done
