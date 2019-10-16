b36e5ee396d4f4d3e619b5ab9d7b3b995c6efbd6
Maxim Prokhorov <prokhorov.max@outlook.com>
2019-10-16 23:48:15 +0300

    test: allow to run a single configuration test, improve commandline handling

--- travis_script.sh
+++ travis_script.sh
@@ -5,7 +5,7 @@ set -e -v
 cd code
 
 if [ ${TRAVIS_BUILD_STAGE_NAME} = "Test" ]; then
-    ./test_build.sh $BUILDER_ENV $BUILDER_EXTRA
+    scripts/test_build.py -e $BUILDER_ENV $BUILDER_EXTRA
 else
     ./build.sh -p
 fi
