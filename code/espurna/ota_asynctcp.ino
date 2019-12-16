47599bf294257becb2d77c41b6e44ba574c68d9a
Maxim Prokhorov <prokhorov.max@outlook.com>
2019-12-16 20:36:29 +0300

    simplify setup and destruction

--- code/espurna/ota_asynctcp.ino
+++ code/espurna/ota_asynctcp.ino
@@ -9,7 +9,7 @@ Copyright (C) 2016-2019 by Xose Pérez <xose dot perez at gmail dot com>
 #if OTA_CLIENT == OTA_CLIENT_ASYNCTCP
 
 // -----------------------------------------------------------------------------
-// Terminal OTA command
+// Terminal and MQTT OTA command handlers
 // -----------------------------------------------------------------------------
 
 #if TERMINAL_SUPPORT || OTA_MQTT_SUPPORT
@@ -22,46 +22,48 @@ Copyright (C) 2016-2019 by Xose Pérez <xose dot perez at gmail dot com>
 
 #include "libs/URL.h"
 
-struct ota_progress_t {
-    enum state_t {
-        HEADERS,
-        DATA,
-        END
-    };
-    state_t state = HEADERS;
-    size_t size = 0;
-};
-
-std::unique_ptr<AsyncClient> _ota_client = nullptr;
-std::unique_ptr<URL> _ota_url = nullptr;
-std::unique_ptr<ota_progress_t> _ota_progress = nullptr;
-
-bool _ota_connected = false;
-
 const char OTA_REQUEST_TEMPLATE[] PROGMEM =
     "GET %s HTTP/1.1\r\n"
     "Host: %s\r\n"
     "User-Agent: ESPurna\r\n"
     "Connection: close\r\n\r\n";
 
+struct ota_client_t {
+    enum state_t {
+        HEADERS,
+        DATA,
+        END
+    };
+
+    ota_client_t(std::unique_ptr<URL>&& url);
+    ota_client_t(const ota_client_t&) = delete;
+
+    bool connect();
+
+    state_t state = HEADERS;
+    size_t size = 0;
+
+    std::unique_ptr<AsyncClient> client;
+    std::unique_ptr<URL> url;
+};
+
+std::unique_ptr<ota_client_t> _ota_client = nullptr;
+
+// -----------------------------------------------------------------------------
+
 void _otaClientDisconnect() {
     DEBUG_MSG_P(PSTR("[OTA] Disconnected\n"));
-    _ota_connected = false;
     _ota_client = nullptr;
-    _ota_url = nullptr;
-    _ota_progress = nullptr;
 }
 
 void _otaClientOnDisconnect(void*, AsyncClient * client) {
     DEBUG_MSG_P(PSTR("\n"));
-    otaFinalize(_ota_progress->size, CUSTOM_RESET_OTA, true);
+    otaFinalize(_ota_client->size, CUSTOM_RESET_OTA, true);
     schedule_function(_otaClientDisconnect);
 }
 
-void _otaClientOnTimeout(void*, AsyncClient *c, uint32_t time) {
-    _ota_connected = false;
-    _ota_url = nullptr;
-    _ota_client->close(true);
+void _otaClientOnTimeout(void*, AsyncClient * client, uint32_t time) {
+    client->close(true);
 }
 
 void _otaClientOnError(void*, AsyncClient* client, err_t error) {
@@ -72,14 +74,14 @@ void _otaClientOnData(void * arg, AsyncClient * client, void * data, size_t len)
 
     auto* ptr = (char *) data;
 
-    if (_ota_progress->state == ota_progress_t::HEADERS) {
+    if (_ota_client->state == ota_client_t::HEADERS) {
         ptr = (char *) strnstr((char *) data, "\r\n\r\n", len);
         if (!ptr) {
             return;
         }
         auto diff = ptr - ((char *) data);
 
-        _ota_progress->state = ota_progress_t::DATA;
+        _ota_client->state = ota_client_t::DATA;
         len -= diff + 4;
         if (!len) {
             return;
@@ -87,15 +89,15 @@ void _otaClientOnData(void * arg, AsyncClient * client, void * data, size_t len)
         ptr += 4;
     }
 
-    if (_ota_progress->state == ota_progress_t::DATA) {
+    if (_ota_client->state == ota_client_t::DATA) {
 
-        if (!_ota_progress->size) {
+        if (!_ota_client->size) {
 
             // Check header before anything is written to the flash
             if (!otaVerifyHeader((uint8_t *) ptr, len)) {
                 DEBUG_MSG_P(PSTR("[OTA] ERROR: No magic byte / invalid flash config"));
                 client->close(true);
-                _ota_progress->state = ota_progress_t::END;
+                _ota_client->state = ota_client_t::END;
                 return;
             }
 
@@ -117,12 +119,12 @@ void _otaClientOnData(void * arg, AsyncClient * client, void * data, size_t len)
         if (Update.write((uint8_t *) ptr, len) != len) {
             otaPrintError();
             client->close(true);
-            _ota_progress->state = ota_progress_t::END;
+            _ota_client->state = ota_client_t::END;
             return;
         }
 
-        _ota_progress->size += len;
-        otaProgress(_ota_progress->size);
+        _ota_client->size += len;
+        otaProgress(_ota_client->size);
 
         delay(0);
 
@@ -130,14 +132,14 @@ void _otaClientOnData(void * arg, AsyncClient * client, void * data, size_t len)
 
 }
 
-void _otaClientOnConnect(void *arg, AsyncClient *client) {
+void _otaClientOnConnect(void*, AsyncClient *client) {
 
     #if ASYNC_TCP_SSL_ENABLED
         int check = getSetting("otaScCheck", OTA_SECURE_CLIENT_CHECK).toInt();
         if ((check == SECURE_CLIENT_CHECK_FINGERPRINT) && (443 == _ota_url->port)) {
             uint8_t fp[20] = {0};
             sslFingerPrintArray(getSetting("otaFP", OTA_FINGERPRINT).c_str(), fp);
-            SSL * ssl = _ota_client->getSSL();
+            SSL * ssl = _ota_client->client->getSSL();
             if (ssl_match_fingerprint(ssl, fp) != SSL_OK) {
                 DEBUG_MSG_P(PSTR("[OTA] Warning: certificate fingerpint doesn't match\n"));
                 client->close(true);
@@ -149,56 +151,54 @@ void _otaClientOnConnect(void *arg, AsyncClient *client) {
     // Disabling EEPROM rotation to prevent writing to EEPROM after the upgrade
     eepromRotate(false);
 
-    DEBUG_MSG_P(PSTR("[OTA] Downloading %s\n"), _ota_url->path.c_str());
-    char buffer[strlen_P(OTA_REQUEST_TEMPLATE) + _ota_url->path.length() + _ota_url->host.length()];
-    snprintf_P(buffer, sizeof(buffer), OTA_REQUEST_TEMPLATE, _ota_url->path.c_str(), _ota_url->host.c_str());
+    DEBUG_MSG_P(PSTR("[OTA] Downloading %s\n"), _ota_client->url->path.c_str());
+    char buffer[strlen_P(OTA_REQUEST_TEMPLATE) + _ota_client->url->path.length() + _ota_client->url->host.length()];
+    snprintf_P(buffer, sizeof(buffer), OTA_REQUEST_TEMPLATE, _ota_client->url->path.c_str(), _ota_client->url->host.c_str());
     client->write(buffer);
 }
 
+ota_client_t::ota_client_t(std::unique_ptr<URL>&& url) :
+    url(std::move(url)), client(new AsyncClient())
+{
+    client->setRxTimeout(5);
+    client->onDisconnect(_otaClientOnDisconnect, nullptr);
+    client->onError(_otaClientOnError, nullptr);
+    client->onTimeout(_otaClientOnTimeout, nullptr);
+    client->onData(_otaClientOnData, nullptr);
+    client->onConnect(_otaClientOnConnect, nullptr);
+}
+
+bool ota_client_t::connect() {
+    #if ASYNC_TCP_SSL_ENABLED
+        return client->connect(url->host.c_str(), url->port, 443 == url->port);
+    #else
+        return client->connect(url->host.c_str(), url->port);
+    #endif
+}
+
+// -----------------------------------------------------------------------------
+
 void _otaClientFrom(const String& url) {
 
-    if (_ota_client || _ota_connected) {
+    if (_ota_client) {
         DEBUG_MSG_P(PSTR("[OTA] Already connected\n"));
         return;
     }
 
-    if (_ota_url) _ota_url = nullptr;
-    _ota_url = std::make_unique<URL>(url);
-
-    // we only support HTTP
-    if ((!_ota_url->protocol.equals("http")) && (!_ota_url->protocol.equals("https"))) {
+    auto _url = std::make_unique<URL>(url);
+    if ((!_url->protocol.equals("http")) && (!_url->protocol.equals("https"))) {
         DEBUG_MSG_P(PSTR("[OTA] Incorrect URL specified\n"));
-        _ota_url = nullptr;
         return;
     }
 
-    // Helper struct to track current data parsing state
-    _ota_progress = std::make_unique<ota_progress_t>();
-
-    _ota_client = std::make_unique<AsyncClient>();
-    _ota_client->setRxTimeout(5);
-    _ota_client->onDisconnect(_otaClientOnDisconnect, nullptr);
-    _ota_client->onError(_otaClientOnError, nullptr);
-    _ota_client->onTimeout(_otaClientOnTimeout, nullptr);
-    _ota_client->onData(_otaClientOnData, nullptr);
-    _ota_client->onConnect(_otaClientOnConnect, nullptr);
-
-    #if ASYNC_TCP_SSL_ENABLED
-        _ota_connected = _ota_client->connect(_ota_url->host.c_str(), _ota_url->port, 443 == _ota_url->port);
-    #else
-        _ota_connected = _ota_client->connect(_ota_url->host.c_str(), _ota_url->port);
-    #endif
-
-    if (!_ota_connected) {
+    _ota_client = std::make_unique<ota_client_t>(std::move(_url));
+    if (!_ota_client->connect()) {
         DEBUG_MSG_P(PSTR("[OTA] Connection failed\n"));
-        _ota_url = nullptr;
-        _ota_client->close(true);
     }
 
 }
 
 #endif // TERMINAL_SUPPORT || OTA_MQTT_SUPPORT
-
 
 #if TERMINAL_SUPPORT
 
