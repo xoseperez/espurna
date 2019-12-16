c6758e48447a2c26b428cc98cdc8192f74136b95
Max Prokhorov <prokhorov.max@outlook.com>
2019-12-16 14:09:52 +0300

    Revert "ir: use proper methods" (#2066)
    This reverts commit 7efaaa8a8054cb4cb3c421ca92a0d98f39922b32.

--- code/espurna/ir.ino
+++ code/espurna/ir.ino
@@ -305,8 +305,7 @@ void _irProcess(unsigned char type, unsigned long code) {
                     }
 
                     if (button_mode == IR_BUTTON_MODE_RGB) {
-                        lightColor((button_value >> 8) & 0xffffff);
-                        lightBrightness(button_value & 0xff);
+                        lightColor(button_value);
                     }
 
                     /*
