ed8d7d92656b750eb1dda8d79d14c2f47fbd481b
Maxim Prokhorov <prokhorov.max@outlook.com>
2019-12-19 16:30:14 +0300

    ha: try to avoid conflict with useCSS

--- code/espurna/homeassistant.ino
+++ code/espurna/homeassistant.ino
@@ -430,6 +430,19 @@ void _haConfigure() {
     const bool enabled = getSetting("haEnabled", HOMEASSISTANT_ENABLED).toInt() == 1;
     _ha_send_flag = (enabled != _ha_enabled);
     _ha_enabled = enabled;
+
+    // https://github.com/xoseperez/espurna/issues/1273
+    // https://gitter.im/tinkerman-cat/espurna?at=5df8ad4655d9392300268a8c
+    // TODO: ensure that this is called before _lightConfigure()
+    //       in case useCSS value is ever cached by the lights module
+    #if LIGHT_PROVIDER != LIGHT_PROVIDER_NONE
+        if (enabled) {
+            if (getSetting("useCSS", LIGHT_USE_CSS).toInt() == 1) {
+                setSetting("useCSS", 0);
+            }
+        }
+    #endif
+
     _haSend();
 }
 
