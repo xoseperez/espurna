c4c45c8d7c5faa7dc7cdd4fb64e905c6dd8a3719
Maxim Prokhorov <prokhorov.max@outlook.com>
2020-02-04 05:45:49 +0300

    ...

--- code/espurna/rpnrules.h
+++ code/espurna/rpnrules.h
@@ -8,6 +8,9 @@ Copyright (C) 2019 by Xose Pérez <xose dot perez at gmail dot com>
 
 #pragma once
 
+// TODO: need this prototype for .ino
+struct rpn_context;
+
 #if RPN_RULES_SUPPORT
 
 #include <rpnlib.h>
