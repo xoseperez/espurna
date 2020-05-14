79ae8cd398bf79518ca1075343e62ae50e42a481
Maxim Prokhorov <prokhorov.max@outlook.com>
2020-05-14 17:09:10 +0300

    no need for embediswrap

--- code/espurna/settings.h
+++ code/espurna/settings.h
@@ -16,7 +16,7 @@ Copyright (C) 2016-2019 by Xose Pérez <xose dot perez at gmail dot com>
 #include <ArduinoJson.h>
 
 #include "espurna.h"
-#include "libs/EmbedisWrap.h"
+#include "Embedis.h"
 
 // --------------------------------------------------------------------------
 
