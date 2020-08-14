923135ccf95bf3b43de0c4ecd091035a6b17bb12
dpeddi <dpeddi@gmail.com>
2020-08-14 21:24:09 +0200

    remove double thermostat.h definition in main.cpp

--- code/espurna/main.cpp
+++ code/espurna/main.cpp
@@ -61,7 +61,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "web.h"
 #include "ws.h"
 #include "mcp23s08.h"
-#include "thermostat.h"
 
 std::vector<void_callback_f> _loop_callbacks;
 std::vector<void_callback_f> _reload_callbacks;
