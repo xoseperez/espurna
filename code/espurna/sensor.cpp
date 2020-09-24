7dd8594a2650838bdc5e9a91c06a2c07a742514f
Maxim Prokhorov <prokhorov.max@outlook.com>
2020-09-24 06:15:28 +0300

    sns: default to SumFilter when counting values
    Simplify fix for:
    https://gitter.im/tinkerman-cat/espurna?at=5f6661a1f969413294f7404d
    
    Most of the time, we care about the sum during report interval, not the
    value that gets aggregated across all readings

--- code/espurna/sensor.cpp
+++ code/espurna/sensor.cpp
@@ -520,17 +520,14 @@ BaseFilter* _magnitudeCreateFilter(unsigned char type, size_t size) {
     case MAGNITUDE_ENERGY:
         filter = new LastFilter();
         break;
+    case MAGNITUDE_COUNT:
+    case MAGNITUDE_GEIGER_CPM:
+    case MAGNITUDE_GEIGER_SIEVERT:
     case MAGNITUDE_ENERGY_DELTA:
         filter = new SumFilter();
         break;
     case MAGNITUDE_DIGITAL:
         filter = new MaxFilter();
-        break;
-    // For geiger counting moving average filter is the most appropriate if needed at all.
-    case MAGNITUDE_COUNT:
-    case MAGNITUDE_GEIGER_CPM:
-    case MAGNITUDE_GEIGER_SIEVERT:
-        filter = new MovingAverageFilter();
         break;
     default:
         filter = new MedianFilter();
@@ -2752,23 +2749,7 @@ void sensorLoop() {
             }
 
             magnitude.last = value_raw;
-
-            // -------------------------------------------------------------
-            // Processing (filters)
-            // -------------------------------------------------------------
-
             magnitude.filter->add(value_raw);
-
-            // Special case for MovingAverageFilter
-            switch (magnitude.type) {
-                case MAGNITUDE_COUNT:
-                case MAGNITUDE_GEIGER_CPM:
-                case MAGNITUDE_GEIGER_SIEVERT:
-                    value_raw = magnitude.filter->result();
-                    break;
-                default:
-                    break;
-            }
 
             // -------------------------------------------------------------
             // Procesing (units and decimals)
