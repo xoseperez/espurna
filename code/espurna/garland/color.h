b48df4b4c7af05cc2ffac4ed3d54162ea63fe0a2
Dmitry Blinov <dblinov@blackberry.com>
2020-12-15 21:39:53 +0200

    Tune color.h

--- code/espurna/garland/color.h
+++ code/espurna/garland/color.h
@@ -10,27 +10,24 @@ struct Color
     byte g;
     byte b;
 
-    inline Color() __attribute__((always_inline)) : r(0), g(0), b(0) {}
-    
+    inline Color() : r(0), g(0), b(0) {}
+
     // allow construction from R, G, B
-    inline Color( uint8_t ir, uint8_t ig, uint8_t ib)  __attribute__((always_inline))
-    : r(ir), g(ig), b(ib)
-    {
+    inline Color(uint8_t ir, uint8_t ig, uint8_t ib)
+        : r(ir), g(ig), b(ib) {
     }
 
     // allow construction from 32-bit (really 24-bit) bit 0xRRGGBB color code
-    inline Color( uint32_t colorcode)  __attribute__((always_inline))
-    : r((colorcode >> 16) & 0xFF), g((colorcode >> 8) & 0xFF), b((colorcode >> 0) & 0xFF)
-    {
+    inline Color(uint32_t colorcode)
+        : r((colorcode >> 16) & 0xFF), g((colorcode >> 8) & 0xFF), b((colorcode >> 0) & 0xFF) {
     }
 
     //interpolates between this color and provided. 
     //x is from 0 to 1, 0 gives this color, 1 gives provided color, values between give interpolation
-    Color interpolate(Color color, float x) const
-    {
-        int r0 = x*(color.r - r) + r;
-        int g0 = x*(color.g - g) + g;
-        int b0 = x*(color.b - b) + b;
+    Color interpolate(Color color, float x) const {
+        int r0 = x * (color.r - r) + r;
+        int g0 = x * (color.g - g) + g;
+        int b0 = x * (color.b - b) + b;
         return Color(r0, g0, b0);
     }
 
@@ -43,34 +40,32 @@ struct Color
     }
 
     //fades (decreases all RGB channels brightness) this color by k
-    void fade(byte k)
-    {
-      if (r>=k) { r=r-k; } else { r=0; }
-      if (g>=k) { g=g-k; } else { g=0; }
-      if (b>=k) { b=b-k; } else { b=0; }
+    void fade(byte k) {
+        if (r>=k) { r=r-k; } else { r=0; }
+        if (g>=k) { g=g-k; } else { g=0; }
+        if (b>=k) { b=b-k; } else { b=0; }
     }
 
     //fades color separately for each channel
-    void fade3(byte dr, byte dg, byte db)
-    {
-      if (r>=dr) { r=r-dr; } else { r=0; }
-      if (g>=dg) { g=g-dg; } else { g=0; }
-      if (b>=db) { b=b-db; } else { b=0; }
+    void fade3(byte dr, byte dg, byte db) {
+        if (r>=dr) { r=r-dr; } else { r=0; }
+        if (g>=dg) { g=g-dg; } else { g=0; }
+        if (b>=db) { b=b-db; } else { b=0; }
     }  
 
     //checks whether this color is visually close to given one
     bool isCloseTo(Color c) const {
-      int diff = abs(r-c.r) + abs(g-c.g) + abs(b-c.b);
-      return diff <= 220; //220 is magic number. Low values give "true" on closer colors, while higher can cause infinite loop while trying to find different color
+        int diff = abs(r - c.r) + abs(g - c.g) + abs(b - c.b);
+        return diff <= 220;  //220 is magic number. Low values give "true" on closer colors, while higher can cause infinite loop while trying to find different color
     }
 
     //return value, that can be used to define how close one color to another
     int howCloseTo(Color c) const {
-      return abs(r-c.r) + abs(g-c.g) + abs(b-c.b);
+        return abs(r - c.r) + abs(g - c.g) + abs(b - c.b);
     }
 
     bool empty() const {
-      return r == 0 && g == 0 && b == 0;
+        return r == 0 && g == 0 && b == 0;
     }
 
     void println() const {
