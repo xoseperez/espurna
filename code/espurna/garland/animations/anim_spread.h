d4b170c5123daaa8333532b5f91beda484109307
Dmitry Blinov <dblinov@blackberry.com>
2020-12-19 15:05:20 +0200

    Refine AnimSpread

--- code/espurna/garland/animations/anim_spread.h
+++ code/espurna/garland/animations/anim_spread.h
@@ -3,17 +3,38 @@
 #include "../anim.h"
 #include "../color.h"
 #include "../palette.h"
-
-#define SPREAD_MAX_WIDTH 20
+#include "debug.h"
 
 //------------------------------------------------------------------------------
 class AnimSpread : public Anim {
+    const byte minWidth = 10;
+    const byte maxWidth = 15;
+    const byte numTries = 5;
+    const byte fade_ind = 255 / minWidth;
+
+    int GeneratePos() {
+        int pos = -1;
+        for (int i = 0; i < numTries; ++i) {
+            pos = secureRandom(0, numLeds);
+            for (int j = pos - maxWidth; j < pos + maxWidth; ++j) {
+                if (j >= 0 && j < numLeds && seq[j] > 0) {
+                    pos = -1;
+                    break;
+                }
+            }
+            if (pos >= 0) break;
+        }
+        return pos;
+    }
+
    public:
     AnimSpread() : Anim("Spread") {
+        cycleFactor = 2;
     }
 
     void SetupImpl() override {
-        inc = secureRandom(2, 8);
+        inc = secureRandom(2, 4);
+        DEBUG_MSG_P(PSTR("[GARLAND] AnimSpread inc = %d\n"), inc);
         for (int i = 0; i < numLeds; i++)
             seq[i] = 0;
     }
@@ -24,7 +45,7 @@ class AnimSpread : public Anim {
 
         for (int i = 0; i < numLeds; i++) {
             if (seq[i] > 0) {
-                byte width = SPREAD_MAX_WIDTH - seq[i];
+                byte width = maxWidth - seq[i];
                 for (int j = i - width; j <= (i + width); j++) {
                     Color c = ledstmp[i];
                     if (j >= 0 && j < numLeds) {
@@ -33,15 +54,18 @@ class AnimSpread : public Anim {
                         leds[j].b += c.b;
                     }
                 }
-                ledstmp[i].fade(255 / SPREAD_MAX_WIDTH);
+                ledstmp[i].fade(fade_ind);
                 seq[i]--;
             }
         }
 
         if (secureRandom(inc) == 0) {
-            byte pos = secureRandom(0, numLeds);
+            int pos = GeneratePos();
+            if (pos == -1)
+                return;
+
             ledstmp[pos] = palette->getRndInterpColor();
-            seq[pos] = SPREAD_MAX_WIDTH;
+            seq[pos] = secureRandom(minWidth, maxWidth);
         }
     }
 };
