9c220bb7f39ecfcc722cffa46b833bbfe3e60a4b
Dmitry Blinov <dblinov@blackberry.com>
2020-12-20 03:43:33 +0200

    Move scene.cpp and anim.cpp to garland.cpp

--- code/espurna/garland.cpp
+++ code/espurna/garland.cpp
@@ -9,8 +9,8 @@ More long strip can take more time to show.
 Currently animation calculation, brightness calculation/transition and showing makes in one loop cycle.
 Debug output shows timings. Overal timing should be not more that 3000 ms.
 
-For longer strips have sense to separate show operation from animation/brightness calculation. Or even
-divide showing for parts and process them in different cycles.
+For longer strips have sense to divide entire strip (pixels) on parts about 100 pixels and show one part
+at a cycle.
 */
 
 #include "garland.h"
@@ -24,7 +24,6 @@ divide showing for parts and process them in different cycles.
 #include "garland/color.h"
 #include "garland/palette.h"
 #include "garland/scene.h"
-#include "web.h"
 #include "ws.h"
 
 const char* NAME_GARLAND_ENABLED        = "garlandEnabled";
@@ -35,9 +34,6 @@ const char* NAME_GARLAND_SWITCH         = "garland_switch";
 const char* NAME_GARLAND_SET_BRIGHTNESS = "garland_set_brightness";
 const char* NAME_GARLAND_SET_SPEED      = "garland_set_speed";
 const char* NAME_GARLAND_SET_DEFAULT    = "garland_set_default";
-
-constexpr int LEDS                      = GARLAND_LEDS;
-constexpr int PIN                       = GARLAND_D_PIN; // WS2812 pin number
 
 #define EFFECT_UPDATE_INTERVAL_MIN      5000  // 5 sec
 #define EFFECT_UPDATE_INTERVAL_MAX      10000 // 10 sec
@@ -89,7 +85,7 @@ Palette pals[] = {
 
 constexpr size_t palsSize() { return sizeof(pals)/sizeof(pals[0]); }
 
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(LEDS, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(GARLAND_LEDS, GARLAND_D_PIN, NEO_GRB + NEO_KHZ800);
 Scene scene(&pixels);
 
 Anim* anims[] = {new AnimStart(), new AnimPixieDust(), new AnimSparkr(), new AnimRun(), new AnimStars(),
@@ -254,5 +250,219 @@ void garlandSetup() {
 
     _interval_effect_update = secureRandom(EFFECT_UPDATE_INTERVAL_MIN, EFFECT_UPDATE_INTERVAL_MAX);
 }
+
+/*#######################################################################
+  _____
+ / ____|
+| (___     ___    ___   _ __     ___ 
+ \___ \   / __|  / _ \ | '_ \   / _ \
+ ____) | | (__  |  __/ | | | | |  __/
+|_____/   \___|  \___| |_| |_|  \___|
+#######################################################################*/
+
+#define GARLAND_SCENE_TRANSITION_MS      1000    // transition time between animations, ms
+#define GARLAND_SCENE_SPEED_MAX          70
+#define GARLAND_SCENE_SPEED_FACTOR       10
+#define GARLAND_SCENE_DEFAULT_SPEED      50
+#define GARLAND_SCENE_DEFAULT_BRIGHTNESS 255
+
+Scene::Scene(Adafruit_NeoPixel* pixels)
+    : _pixels(pixels),
+      _numLeds(pixels->numPixels()),
+      _leds1(_numLeds),
+      _leds2(_numLeds),
+      _ledstmp(_numLeds),
+      _seq(_numLeds) {
+}
+
+void Scene::setPalette(Palette* palette) {
+    _palette = palette;
+    if (setUpOnPalChange) {
+        setupImpl();
+    }
+}
+
+void Scene::setBrightness(byte brightness) {
+    DEBUG_MSG_P(PSTR("[GARLAND] Scene::setBrightness = %d\n"), brightness);
+    this->brightness = brightness;
+}
+
+byte Scene::getBrightness() {
+    DEBUG_MSG_P(PSTR("[GARLAND] Scene::getBrightness = %d\n"), brightness);
+    return brightness;
+}
+
+// Speed is reverse to cycleFactor and 10x
+void Scene::setSpeed(byte speed) {
+    this->speed = speed;
+    cycleFactor = (float)(GARLAND_SCENE_SPEED_MAX - speed) / GARLAND_SCENE_SPEED_FACTOR;
+    DEBUG_MSG_P(PSTR("[GARLAND] Scene::setSpeed %d cycleFactor = %d\n"), speed, (int)(cycleFactor * 1000));
+}
+
+byte Scene::getSpeed() {
+    DEBUG_MSG_P(PSTR("[GARLAND] Scene::getSpeed %d cycleFactor = %d\n"), speed, (int)(cycleFactor * 1000));
+    return speed;
+}
+
+void Scene::setDefault() {
+    speed = GARLAND_SCENE_DEFAULT_SPEED;
+    cycleFactor = (float)(GARLAND_SCENE_SPEED_MAX - speed) / GARLAND_SCENE_SPEED_FACTOR;
+    brightness = GARLAND_SCENE_DEFAULT_BRIGHTNESS;
+    DEBUG_MSG_P(PSTR("[GARLAND] Scene::setDefault speed = %d cycleFactor = %d brightness = %d\n"), speed, (int)(cycleFactor * 1000), brightness);
+}
+
+void Scene::run() {
+    unsigned long iteration_start_time = micros();
+
+    if (state == Calculate || cyclesRemain < 1) {
+        // Calculate number of cycles for this animation iteration
+        float cycleSum = cycleFactor * (_anim ? _anim->getCycleFactor() : 1.0) + cycleTail;
+        cyclesRemain = cycleSum;
+        if (cyclesRemain < 1) {
+            cyclesRemain = 1;
+            cycleSum = 0;
+            cycleTail = 0;
+        } else {
+            cycleTail = cycleSum - cyclesRemain;
+        }
+
+        if (_anim) {
+            _anim->Run();
+        }
+
+        sum_calc_time += (micros() - iteration_start_time);
+        iteration_start_time = micros();
+        ++calc_num;
+        state = Transition;
+    }
+    
+    if (state == Transition && cyclesRemain < 3) {
+        // transition coef, if within 0..1 - transition is active
+        // changes from 1 to 0 during transition, so we interpolate from current
+        // color to previous
+        float transc = (float)((long)transms - (long)millis()) / GARLAND_SCENE_TRANSITION_MS;
+        Color* leds_prev = (_leds == &_leds1[0]) ? &_leds2[0] : &_leds1[0];
+
+        if (transc > 0) {
+            for (int i = 0; i < _numLeds; i++) {
+                // transition is in progress
+                Color c = _leds[i].interpolate(leds_prev[i], transc);
+                byte r = (int)(bri_lvl[c.r]) * brightness / 256;
+                byte g = (int)(bri_lvl[c.g]) * brightness / 256;
+                byte b = (int)(bri_lvl[c.b]) * brightness / 256;
+                _pixels->setPixelColor(i, _pixels->Color(r, g, b));
+            }
+        } else {
+            for (int i = 0; i < _numLeds; i++) {
+                // regular operation
+                byte r = (int)(bri_lvl[_leds[i].r]) * brightness / 256;
+                byte g = (int)(bri_lvl[_leds[i].g]) * brightness / 256;
+                byte b = (int)(bri_lvl[_leds[i].b]) * brightness / 256;
+                _pixels->setPixelColor(i, _pixels->Color(r, g, b));
+            }
+        }
+
+        sum_pixl_time += (micros() - iteration_start_time);
+        iteration_start_time = micros();
+        ++pixl_num;
+        state = Show;
+    }
+    
+    if (state == Show && cyclesRemain < 2) {    
+        _pixels->show();
+        sum_show_time += (micros() - iteration_start_time);
+        ++show_num;
+        state = Calculate;
+        ++numShows;
+    }
+    --cyclesRemain;
+}
+
+void Scene::setupImpl() {
+    transms = millis() + GARLAND_SCENE_TRANSITION_MS;
+
+    // switch operation buffers (for transition to operate)
+    if (_leds == &_leds1[0]) {
+        _leds = &_leds2[0];
+    } else {
+        _leds = &_leds1[0];
+    }
+
+    if (_anim) {
+        _anim->Setup(_palette, _numLeds, _leds, &_ledstmp[0], &_seq[0]);
+    }
+}
+
+void Scene::setup() {
+    sum_calc_time = 0;
+    sum_pixl_time = 0;
+    sum_show_time = 0;
+    calc_num = 0;
+    pixl_num = 0;
+    show_num = 0;
+    numShows = 0;
+
+    if (!setUpOnPalChange) {
+        setupImpl();
+    }
+}
+
+unsigned long Scene::getAvgCalcTime() { return sum_calc_time / calc_num; }
+unsigned long Scene::getAvgPixlTime() { return sum_pixl_time / pixl_num; }
+unsigned long Scene::getAvgShowTime() { return sum_show_time / show_num; }
+
+/*#######################################################################
+                    _                       _     _
+    /\             (_)                     | |   (_)
+   /  \     _ __    _   _ __ ___     __ _  | |_   _    ___    _ __  
+  / /\ \   | '_ \  | | | '_ ` _ \   / _` | | __| | |  / _ \  | '_ \ 
+ / ____ \  | | | | | | | | | | | | | (_| | | |_  | | | (_) | | | | |
+/_/    \_\ |_| |_| |_| |_| |_| |_|  \__,_|  \__| |_|  \___/  |_| |_|
+#######################################################################*/
+
+Anim::Anim(const char* name) : _name(name) {}
+
+void Anim::Setup(Palette* palette, uint16_t numLeds, Color* leds, Color* ledstmp, byte* seq) {
+    this->palette = palette;
+    this->numLeds = numLeds;
+    this->leds = leds;
+    this->ledstmp = ledstmp;
+    this->seq = seq;
+    SetupImpl();
+}
+
+void Anim::glowSetUp() {
+    braPhaseSpd = secureRandom(4, 13);
+    if (braPhaseSpd > 8) {
+        braPhaseSpd = braPhaseSpd - 17;
+    }
+    braFreq = secureRandom(20, 60);
+}
+
+void Anim::glowForEachLed(int i) {
+    int8 bra = braPhase + i * braFreq;
+    bra = BRA_OFFSET + (abs(bra) >> BRA_AMP_SHIFT);
+    leds[i] = leds[i].brightness(bra);
+}
+
+void Anim::glowRun() { braPhase += braPhaseSpd; }
+
+bool operator== (const Color &c1, const Color &c2)
+{
+    return (c1.r == c2.r && c1.g == c2.g && c1.b == c2.b);
+}
+
+unsigned int rng() {
+    static unsigned int y = 0;
+    y += micros();  // seeded with changing number
+    y ^= y << 2;
+    y ^= y >> 7;
+    y ^= y << 7;
+    return (y);
+}
+
+// Ranom numbers generator in byte range (256) much faster than secureRandom.
+// For usage in time-critical places.
+byte rngb() { return (byte)rng(); }
 
 #endif  // GARLAND_SUPPORT
