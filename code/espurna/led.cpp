ec65393bc362985c8224b040c6196f1c5ebbcbe2
Maxim Prokhorov <prokhorov.max@outlook.com>
2021-01-02 09:59:55 +0300

    check how leds behave

--- code/espurna/led.cpp
+++ code/espurna/led.cpp
@@ -23,16 +23,14 @@ Copyright (C) 2016-2019 by Xose Pérez <xose dot perez at gmail dot com>
 
 // LED helper class
 
-led_t::led_t(unsigned char pin, bool inverse, unsigned char mode, unsigned char relayID) :
-    pin(pin),
-    inverse(inverse),
-    mode(mode),
-    relayID(relayID)
+led_t::led_t(unsigned char pin_, bool inverse_, unsigned char mode_, unsigned char relayID_) :
+    pin(pin_),
+    inverse(inverse_),
+    mode(mode_),
+    relayID(relayID_)
 {
-    if (pin != GPIO_NONE) {
-        pinMode(pin, OUTPUT);
-        status(false);
-    }
+    pinMode(pin, OUTPUT);
+    status(false);
 }
 
 bool led_t::status() {
@@ -462,15 +460,14 @@ void ledSetup() {
 
     for (unsigned char index=0; index < LedsMax; ++index) {
         const auto pin = getSetting({"ledGPIO", index}, _ledPin(index));
-        if (!gpioValid(pin)) {
-            break;
+        if (gpioLock(pin)) {
+            _leds.emplace_back(
+                pin,
+                getSetting({"ledInv", index}, _ledInverse(index)),
+                getSetting({"ledMode", index}, _ledMode(index)),
+                getSetting({"ledRelay", index}, _ledRelay(index))
+            );
         }
-        _leds.emplace_back(
-            pin,
-            getSetting({"ledInv", index}, _ledInverse(index)),
-            getSetting({"ledMode", index}, _ledMode(index)),
-            getSetting({"ledRelay", index}, _ledRelay(index))
-        );
     }
 
     _led_update = true;
