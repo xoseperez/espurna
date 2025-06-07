2b4c619b0d76fa36f2a83f9dde0a3f6d6290b2c0
Hamed Ta <hamed.t@live.com>
2025-06-07 19:37:36 +0930

    Added missed semicolon

--- code/espurna/garland/animations/color_wave.h
+++ code/espurna/garland/animations/color_wave.h
@@ -85,7 +85,7 @@ class ColorWave {
         uint16_t headDelta = head - prevHead;
 
         if (params.pixelCache && headDelta > 0) {
-            for (size_t i = params.numLeds; i-- > 0) {
+            for (size_t i = params.numLeds; i-- > 0;) {
                 if (i >= headDelta) {
                     params.pixelCache[i] = params.pixelCache[i - headDelta];
                 } else {
