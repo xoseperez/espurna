7fc33fdcc3b067b7b2a8a4767a851fac5ed04659
Maxim Prokhorov <prokhorov.max@outlook.com>
2026-04-13 14:36:17 +0300

    webui(vitest): resolve build preset imports

--- code/vitest.config.mjs
+++ code/vitest.config.mjs
@@ -1,9 +1,24 @@
+import * as path from 'node:path';
+
+import { MODULE_DEV } from './html/preset.mjs';
+import {
+    PRESET_DIR,
+    SPEC_DIR,
+    VENDOR_DIR,
+} from './gulpfile.mjs';
+
 import { defineConfig } from 'vitest/config';
-import { SPEC_DIR } from './gulpfile.mjs';
 
 export default defineConfig({
     test: {
         environment: 'jsdom',
         dir: SPEC_DIR,
-    }
+    },
+    // vite-specific overrides, both for the build and importmap helper script
+    resolve: {
+        alias: [
+            {find: '/vendor', replacement: VENDOR_DIR},
+            {find: '@build-preset', replacement: path.join(PRESET_DIR, MODULE_DEV)},
+        ],
+    },
 });
